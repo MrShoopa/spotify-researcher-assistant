12eea947750c7ace87d4feb8ca7927b229711c84
Joe V <joevillegasisawesome@gmail.com>
2019-03-14 00:54:53 -0700

    Documented the basis of the Spotify API Handler

--- src/components/api/SpotifyDataHandler.jsx
+++ src/components/api/SpotifyDataHandler.jsx
@@ -1,12 +1,47 @@
+/* 
+    Handles authentication and data retreival with Spotify API
+    For use with online React web applications.
+    
+    @reference  (https://developer.spotify.com/documentation/web-api/)
+    @author     Joe Villegas (joev@uw.edu)
+    @date       3/14/19
+*/
+
+//  Internal Components
 import React from 'react'
 import Axios from 'axios'
+import AxiosRetry from 'axios-retry'
 
-import auth from '../resources/auth.json'
+//  Resources
+import auth from '../resources/auth.json'   //  Must include valid IDs before methods are called
+
+AxiosRetry(Axios, { retries: 5 });  //  Retry when API calls fail
 
 class SpotifyDataHandler extends React.Component {
 
+    errorInterceptor = () => Axios.interceptors.response.use(
+        response => response,
+        error => {
+            const status = error.response;
+            if (status === 404) {
+                console.err('Resource not found')
+                // Custom function
+            }
+            return Promise.reject(error);
+        }
+    );
+
+    authenticate = () => {
+
+    }
 
 
+    componentDidMount = () => {
+        this.props.onRef(this)
+    }
+    componentWillUnmount = () => {
+        //this.props.onRef(undefined)
+    }
 
     render() {
         return <p></p>
