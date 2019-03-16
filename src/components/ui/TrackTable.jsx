4ff027e4638e4c5429d2d142517036166c5ebb5e
Joe V <joevillegasisawesome@gmail.com>
2019-03-16 02:08:36 -0700

    Corrected seconds output

--- src/components/ui/TrackTable.jsx
+++ src/components/ui/TrackTable.jsx
@@ -5,24 +5,29 @@ function TrackTable(props) {
         <table>
             <thead>
                 <th>
-                    <button onClick={() => props.sortBy('artist')} />
-                    Artist
+                    <button onClick={() => props.sortBy('artist')}>
+                        Artist
+                    </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('title')} />
-                    Title
+                    <button onClick={() => props.sortBy('title')}>
+                        Title
+                    </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('energy')} />
-                    Energy
+                    <button onClick={() => props.sortBy('energy')}>
+                        Energy
+                    </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('valence')} />
-                    Valence
+                    <button onClick={() => props.sortBy('valence')}>
+                        Valence
+                    </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('duration_ms')} />
-                    Seconds
+                    <button onClick={() => props.sortBy('duration_ms')}>
+                        Seconds
+                    </button>
                 </th>
             </thead>
             <tbody>
@@ -34,7 +39,7 @@ function TrackTable(props) {
                                 <td>{track.title}</td>
                                 <td>{track.energy}</td>
                                 <td>{track.valence}</td>
-                                <td>{Math.floor((track.duration_ms / 1000) % 60)}</td>
+                                <td>{Math.floor((track.duration_ms / 1000))}</td>
                             </tr>
                         )
                     )
