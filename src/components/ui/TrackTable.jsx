32c4d7a8169ab2d89026a718c8301f6ae7944f5c
Joe V <joevillegasisawesome@gmail.com>
2019-03-16 22:46:27 -0700

    Added color coding functionality based on energy

--- src/components/ui/TrackTable.jsx
+++ src/components/ui/TrackTable.jsx
@@ -1,43 +1,45 @@
 import React from 'react'
 
+import './TrackTable.scss'
+
 function TrackTable(props) {
 
+    // Generates Red-Green color based on track's 'energy' value.
     function determineStrengthColor(nrg_val) {
-        if (nrg_val < .2) return 'energy-0'
-        else if (nrg_val < .4) return 'energy-1'
-        else if (nrg_val < .6) return 'energy-2'
-        else if (nrg_val < .8) return 'energy-3'
-        else if (nrg_val < .1) return 'energy-4'
-        else if (nrg_val < .1) return 'energy-max'
-        else return 'energy-unknown'
+        const green = 120
 
+        var hue = green * (1 - nrg_val)
+
+        // Return a CSS HSL string
+        return 'hsl(' + hue + ', 100%, 30%)';
     }
 
+    // Table of tracks featuring five details as listed below.
     return (
-        <table>
+        <table id='track-table'>
             <thead>
                 <th>
-                    <button onClick={() => props.sortBy('artist')}>
+                    <button type='sort' onClick={() => props.sortBy('artist')}>
                         Artist
                     </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('title')}>
+                    <button type='sort' onClick={() => props.sortBy('title')}>
                         Title
                     </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('energy')}>
+                    <button type='sort' onClick={() => props.sortBy('energy')}>
                         Energy
                     </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('valence')}>
+                    <button type='sort' onClick={() => props.sortBy('valence')}>
                         Valence
                     </button>
                 </th>
                 <th>
-                    <button onClick={() => props.sortBy('duration_ms')}>
+                    <button type='sort' onClick={() => props.sortBy('duration_ms')}>
                         Seconds
                     </button>
                 </th>
@@ -46,7 +48,8 @@ function TrackTable(props) {
                 {
                     props.track_list.map(track =>
                         (
-                            <tr className={() => this.determineStrengthColor(track.energy)}>
+                            <tr className="track-table-item"
+                                style={{ backgroundColor: determineStrengthColor(track.energy) }}>
                                 <td>{track.artist}</td>
                                 <td>{track.title}</td>
                                 <td>{track.energy}</td>
