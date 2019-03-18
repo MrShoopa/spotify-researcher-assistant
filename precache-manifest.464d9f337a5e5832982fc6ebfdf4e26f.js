d3120ca958160c1ab20e17cfa3a24305f4ca89b7
Joe V <joevillegasisawesome@gmail.com>
2019-03-17 20:56:37 -0700

    Updates

---
+++ precache-manifest.464d9f337a5e5832982fc6ebfdf4e26f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8499c8e5f2c80a427f7695acd9d4bcf9",
+    "url": "/static/media/logo.8499c8e5.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b6b6cea0882c0987ca10",
+    "url": "/static/js/main.9184b477.chunk.js"
+  },
+  {
+    "revision": "028d77c49dfdf79963fb",
+    "url": "/static/js/2.ca085e4d.chunk.js"
+  },
+  {
+    "revision": "b6b6cea0882c0987ca10",
+    "url": "/static/css/main.b0559b4b.chunk.css"
+  },
+  {
+    "revision": "17acf386512f257d7715d0a65658cdad",
+    "url": "/index.html"
+  }
+];
