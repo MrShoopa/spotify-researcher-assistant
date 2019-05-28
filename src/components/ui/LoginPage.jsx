e1421792ef976a4a341895a64ff40d364976de2b
Joe V <joev@uw.edu>
2019-05-27 21:15:48 -0700

    Changed login text

--- src/components/ui/LoginPage.jsx
+++ src/components/ui/LoginPage.jsx
@@ -18,12 +18,16 @@ class LoginPage extends Component {
         const url =
             `https://accounts.spotify.com/authorize?client_id=${clientID}&redirect_uri=${redirectURI}&scope=${scope}&response_type=${responseType}&state=${state}`
         return (
-            <div className="login">
+            <div className="login" style={{ width: '-webkit-fill-available' }}>
                 <Form horizontal>
-                    <h1>Welcome to Playlist Recommendation</h1>
+                    <h1>
+                        <span role='img' aria-label='Music'>🎶</span>
+                        Welcome to Playlist Spotter
+                        <span role='img' aria-label='Music'>🎶</span>
+                    </h1>
                     <br />
                     <FormGroup controlId="formHorizontalEmail">
-                        <h4>Please press continue to sign in with Spotify</h4>
+                        <h4>Sign in to Spotify to learn about your playlists and get recommendations!</h4>
                         <Button bsStyle="success" href={url} >Continue</Button>
                     </FormGroup>
                 </Form>
