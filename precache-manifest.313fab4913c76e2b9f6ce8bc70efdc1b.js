0c72cf2a5d2f65dca0b9f082d8e276ab5366f4b8
Joe Villegas <joev@uw.edu>
2019-05-28 18:48:11 -0700

    Updates

--- precache-manifest.313fab4913c76e2b9f6ce8bc70efdc1b.js
+++ precache-manifest.313fab4913c76e2b9f6ce8bc70efdc1b.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/spotify-researcher-assistant/static/js/runtime~main.8a1e847b.js"
   },
   {
-    "revision": "19906d7fc0297dceb118",
-    "url": "/spotify-researcher-assistant/static/js/main.e748bed8.chunk.js"
+    "revision": "2d1401cc74abcfac2281",
+    "url": "/spotify-researcher-assistant/static/js/main.81871c91.chunk.js"
   },
   {
     "revision": "1a311c114aeee623ab61",
     "url": "/spotify-researcher-assistant/static/js/2.0e7900a4.chunk.js"
   },
   {
-    "revision": "19906d7fc0297dceb118",
+    "revision": "2d1401cc74abcfac2281",
     "url": "/spotify-researcher-assistant/static/css/main.87e64a9e.chunk.css"
   },
   {
-    "revision": "4ef0a53bc566000c4ee0cd70d44ec15b",
+    "revision": "8a3830aba6307e7a2e5026553de8c0c6",
     "url": "/spotify-researcher-assistant/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/spotify-researcher-assistant/precache-manifest.cea1a63773bb6cb5950e2f86a67402d2.js"
+  "/spotify-researcher-assistant/precache-manifest.313fab4913c76e2b9f6ce8bc70efdc1b.js"
 );
 
 workbox.clientsClaim();
