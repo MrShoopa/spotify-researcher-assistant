edb83a336a54e0522bf173716fe8195be1568705
Joe V <joev@uw.edu>
2019-06-05 00:10:37 -0700

    Updates

---
+++ precache-manifest.59ae480d9df8e82806e8be5478154748.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8499c8e5f2c80a427f7695acd9d4bcf9",
+    "url": "/spotify-researcher-assistant/static/media/logo.8499c8e5.svg"
+  },
+  {
+    "revision": "f92380fa24316dad42a0",
+    "url": "/spotify-researcher-assistant/static/js/runtime~main.8a1e847b.js"
+  },
+  {
+    "revision": "14460c7c441fee506a53",
+    "url": "/spotify-researcher-assistant/static/js/main.83378aa5.chunk.js"
+  },
+  {
+    "revision": "1bff85af67ca10be36ee",
+    "url": "/spotify-researcher-assistant/static/js/2.74d7a388.chunk.js"
+  },
+  {
+    "revision": "14460c7c441fee506a53",
+    "url": "/spotify-researcher-assistant/static/css/main.7c1f4e75.chunk.css"
+  },
+  {
+    "revision": "88a5b2c0e8dd232a03124a377c040819",
+    "url": "/spotify-researcher-assistant/index.html"
+  }
+];
